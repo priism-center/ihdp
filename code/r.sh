5c3d6be7a655d8a3ecf6873d902b163b5c8085af
Alan Z Chen <alanzchen7@gmail.com>
2019-07-17 13:46:51 -0400

    add hpc script for mgen_1_wr model

---
+++ code/r.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --time=4:00:00
+#SBATCH --mem=32GB
+#SBATCH --nodes=20
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+
+# Load modules
+module purge
+module load r/intel/3.6.0
+
+# cd to repo directory
+REPO=/home/azc211/github/ihdp
+cd $REPO
+
+# Command line arguments
+export R_SCRIPT=$1
+shift
+export ARGS=$@
+
+# Run program
+echo "Rscript $R_SCRIPT $ARGS"
+Rscript $R_SCRIPT $ARGS
