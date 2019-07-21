34266486210449f21752911b59580df50121e0cd
Alan Z Chen <alanzchen7@gmail.com>
2019-07-21 14:47:08 -0400

    fix slurm command

--- code/r.sh
+++ code/r.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=10
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --output=slurm/slurm_$j.out
+#SBATCH --output=slurm/slurm_%j.out
 #SBATCH --mail-type=END
 #SBATCH --mail-user=azc211@nyu.edu
 
