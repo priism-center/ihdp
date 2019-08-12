7eaebc1f602956200953f04aa2d8f9d135858181
Alan Chen <alanzchen7@gmail.com>
2019-08-12 08:49:59 -0400

    update with new ps_fit_2 spec using bw*income

--- code/model/genmatch.r
+++ code/model/genmatch.r
@@ -10,10 +10,10 @@ args <- commandArgs(trailingOnly=TRUE)
 
 if (length(args)==0){
     REP <- FALSE
-    model_file <- 'models/mgen_1.rds'
+    model_file <- 'models/mgen_2.rds'
 } else {
     REP <- TRUE
-    model_file <- 'models/mgen_1_wr.rds'
+    model_file <- 'models/mgen_2_wr.rds'
 }
 
 ############################################
@@ -25,7 +25,10 @@ library(Matching)
 
 load('data/cc2.Rdata')
 
-ps_form <- formula(treat ~ bwg + hispanic + black + b.marr + lths + hs + ltcoll + work.dur + prenatal + sex + first + st5 + st9 + st12 + st25 + st36 + st42 + st48 + st53 + age + bw + preterm + momage + dayskidh)
+# Covs to adjust
+ps_form <- formula(treat ~ bwg + hispanic + black + b.marr + lths + hs + ltcoll + work.dur + prenatal + sex + first + st5 + st9 + st12 + st25 + st36 + st42 + st48 + st53 + bw + preterm + momage + dayskidh + income)
+# Covs to balance
+bal_form <- update(covs, . ~ . - st5 - st9 - st12 - st25 - st36 - st42 - st48 - st53)
 
 ############################################
 
@@ -33,8 +36,9 @@ set.seed(20)
 cl <- makePSOCKcluster(detectCores(), type='PSOCK')
 
 X <- setdiff(all.vars(ps_form), 'treat')
+X_bal <- setdiff(all.vars(bal_form), 'treat')
 
-mgen_mod <- GenMatch(Tr=cc2$treat, X=cc2[,X], BalanceMatrix=cc2[,X], estimand='ATT', pop.size=1000, cluster=cl, replace=REP)
+mgen_mod <- GenMatch(Tr=cc2$treat, X=cc2[,X], BalanceMatrix=cc2[,X_bal], estimand='ATT', pop.size=1000, cluster=cl, replace=REP)
 
 stopCluster(cl)
 
