8cbca228ae93439529bb4cc3be8a345dcc9d576d
Alan Chen <alanzchen7@gmail.com>
2019-08-17 19:30:53 -0400

    update covs

--- code/ps_fitter.r
+++ code/ps_fitter.r
@@ -63,27 +63,32 @@ covs_bal_st <- c(covs_bal, 'st5', 'st9', 'st12', 'st25', 'st36', 'st42', 'st48',
 covs_ps <- c('bw', 'preterm', 'dayskidh', 'sex', 'first', 'bwg', 'black', 'hispanic', 'b.marr', 'lths', 'hs', 'ltcoll', 'work.dur', 'prenatal', 'momage', 'income', 'bwT', 'dayskidT', 'pretermT', 'momageT', 'income')
 # Create all two way two-way interactions
 n <- length(covs_ps)
-idx <- unlist(lapply(1:n, function(i) combn(1:n, 2, simplify=FALSE)),recursive=FALSE)
+idx <- unique(unlist(lapply(1:n, function(i) combn(1:n, 2, simplify=FALSE)),recursive=FALSE))
 covs_int <- sapply(idx, function(i) paste(covs_ps[i], collapse=':'))
+rm(idx)
+gc()
 
 
 ############################################
 # Create all propensity score specifications
-covs_1 <- c(covs_ps, covs_int)
-
-n <- length(covs_1)
-
-idx <- unlist(
-    lapply(1:n, function(i)
-        combn(1:n, i, simplify=FALSE)
-    ), recursive=FALSE)
 
 if (file.exists('outputs/ps_specs.rds')){
     ps_specs <- readRDS('outputs/ps_specs.rds')
 } else {
-    ps_specs <- sapply(idx, function(i)
-        paste0('treat~', paste(covs_1[i], collapse='+'))
-        )
+    covs_1 <- c(covs_ps, 'black:bw', 'black:preterm', 'black:dayskidhT', 'b.marr:bw', 'b.marr:preterm', 'b.marr:dayskidhT', 'b.marr:black', 'bw:income', 'black:income')
+    n <- length(covs_1)
+
+    idx <- unlist(
+        mclapply(6:n, function(i)
+                combn(1:n, i, simplify=FALSE),
+            mc.cores=detectCores()),
+        recursive=FALSE)
+    ps_specs <- mclapply(idx, function(i)
+            paste0('treat~', paste(covs_1[i], collapse='+')),
+        mc.cores=detectCores())
+    rm(idx)
+    gc()
+    saveRDS(ps_specs, 'outputs/ps_specs.rds')
 }
 
 # Estimation
@@ -91,8 +96,5 @@ if (file.exists('outputs/ps_bals.rds')){
     ps_bals <- readRDS('outputs/ps_bals.rds')
 } else {
     ps_bals <- mclapply(ps_specs, function(spec) psBal(spec), mc.cores=detectCores())
+    saveRDS(ps_bals, 'outputs/ps_bals.rds')
 }
-
-# Save everything
-saveRDS(ps_specs, 'outputs/ps_specs.rds')
-saveRDS(ps_bals, 'outputs/ps_bals.rds')
