cdd3145b8c63bfd48d60ad707e8b4ddfb7375c62
Alan Chen <alanzchen7@gmail.com>
2019-08-21 12:28:21 -0400

    add bal.tab convenience function for IHDP data

--- code/library.r
+++ code/library.r
@@ -11,6 +11,8 @@ if(Sys.getenv("SLURM_CPUS_PER_TASK") != "") {
   options(mc.cores=parallel::detectCores()-1)
 }
 
+library(cobalt)
+
 
 ############################################
 
@@ -31,4 +33,14 @@ findMax <- function(i, mwr=TRUE){
 
     idx <- which.max(bals_m)
     return(specs[idx])
+}
+
+
+#' Specific IHDP balance table
+#' 
+#' @param data data.frame of matched sample
+bal_ihdp <- function(data){
+    # Covariates to check balance on
+    covs <- c('bw', 'bwg', 'preterm', 'dayskidh', 'sex', 'first', 'black', 'hispanic', 'b.marr', 'lths', 'hs', 'ltcoll', 'work.dur', 'prenatal', 'momage', 'income')
+    bal.tab(data=data, treat=data$treat, covs=data[,covs], estimand='ATT', m.threshold=0.1, v.threshold=1.1, continuous='std', binary='raw')
 }
