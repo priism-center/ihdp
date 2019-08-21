01a64ac258527c9e1679bf694779ea6391587069
Alan Chen <alanzchen7@gmail.com>
2019-08-20 20:07:56 -0400

    fix typo

--- code/test.r
+++ code/test.r
@@ -2,7 +2,7 @@
 # Test script
 
 ############################################
-args <- commandArgs(trainlingOnly=TRUE)
+args <- commandArgs(trailingOnly=TRUE)
 
 fileConn <- file('test.txt')
 writeLines(c("Hello World", args), fileconn)
