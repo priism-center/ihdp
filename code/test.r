459fe07ae1aee76a92ca06df2d79228247349bb7
Alan Chen <alanzchen7@gmail.com>
2019-08-24 11:13:07 -0400

    update test script to test stan_glm

--- code/test.r
+++ code/test.r
@@ -2,8 +2,21 @@
 # Test script
 
 ############################################
-args <- commandArgs(trailingOnly=TRUE)
 
-fileConn <- file('test.txt')
-writeLines(c("Hello World", args), fileConn)
-close(fileConn)
+library(rstanarm)
+    cpus_per_task <- Sys.getenv('SLURM_CPUS_PER_TASK')
+    if(cpus_per_task!=''){
+        options(mc.cores=as.integer(cpus_per_task))
+    } else {
+        options(mc.cores=parallel::detectCores() - 1)
+    }
+
+############################################
+
+set.seed(88)
+
+df <- data.frame(x=rnorm(100, 8, 1), y=rnorm(100, 2*x, 3))
+
+mod_test <- stan_glm(y ~ x, data=df, family=gaussian(link='identity'), algorithm='optimizing')
+
+summary(mod_test)
